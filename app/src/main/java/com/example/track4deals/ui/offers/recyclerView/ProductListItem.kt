81b4bc5d57561d4c65e5e8c4c6e33f75016f96a3
Valentino Di Cianni <valentino.dicianni@edu.unito.it>
2021-01-12 17:55:26 +0100

    risolto problema di layout e modificato listItem

--- app/src/main/java/com/example/track4deals/ui/offers/recyclerView/ProductListItem.kt
+++ app/src/main/java/com/example/track4deals/ui/offers/recyclerView/ProductListItem.kt
@@ -1,5 +1,10 @@
 package com.example.track4deals.ui.offers.recyclerView
 
+import android.annotation.SuppressLint
+import android.text.Spannable
+import android.text.Spanned
+import android.text.style.StrikethroughSpan
+import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
 import com.example.track4deals.R
@@ -12,11 +17,19 @@ import kotlinx.android.synthetic.main.element_row_rv.view.*
 class ProductListItem(
     var productEntity: ProductEntity
 ) : Item() {
+    private val STRIKE_THROUGH_SPAN = StrikethroughSpan()
 
+    @SuppressLint("SetTextI18n")
     override fun bind(viewHolder: ViewHolder, position: Int) {
         viewHolder.apply {
             viewHolder.itemView.productName.text = productEntity.title
             viewHolder.itemView.productDescription.text = productEntity.description
+            viewHolder.itemView.brand.text = productEntity.brand
+            viewHolder.itemView.oldPrice.setText("${productEntity.normal_price} €", TextView.BufferType.SPANNABLE)
+            val text = viewHolder.itemView.oldPrice.text as Spannable
+            text.setSpan(STRIKE_THROUGH_SPAN, 0, text.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+
+            viewHolder.itemView.newPrice.text =  "${productEntity.offer_price} €"
             updateImage()
         }
     }
