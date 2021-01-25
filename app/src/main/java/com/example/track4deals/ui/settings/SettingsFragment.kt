6d7e2c2f15e1d1b86fab0a81023d28bc6ded635f
Valentino Di Cianni <valentino.dicianni@edu.unito.it>
2021-01-25 22:55:48 +0100

    dummy enable notifications done (not working, is a mess)

--- app/src/main/java/com/example/track4deals/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/track4deals/ui/settings/SettingsFragment.kt
@@ -4,32 +4,28 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Toast
-import android.widget.ToggleButton
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.app.NotificationManagerCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
-import androidx.preference.PreferenceManager
 import com.example.track4deals.R
 import com.example.track4deals.internal.UserProvider
 import com.google.firebase.auth.FirebaseAuth
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.closestKodein
 import org.kodein.di.generic.instance
-import kotlin.math.log
 
 
 class SettingsFragment : PreferenceFragmentCompat(), KodeinAware {
     override val kodein by closestKodein()
     private val userProvider by instance<UserProvider>()
     private lateinit var settingsViewModel: SettingsViewModel
-    private lateinit var manager: PreferenceManager
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
 
     }
-
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -51,12 +47,40 @@ class SettingsFragment : PreferenceFragmentCompat(), KodeinAware {
             }
             if (preference.key == getString(R.string.Theme)) {
                 if (preference.isEnabled) {
-                    if(AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES)
+                    if (AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES) {
                         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-                    else
+                        Toast.makeText(
+                            context,
+                            getString(R.string.enableLightTheme),
+                            Toast.LENGTH_LONG
+                        ).show()
+                    } else {
                         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-
-
+                        Toast.makeText(
+                            context,
+                            getString(R.string.enableDarkTheme),
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+            }
+            if (preference.key == getString(R.string.notifications)) {
+                if (preference.isEnabled) {
+                    if (NotificationManagerCompat.from(requireContext())
+                            .areNotificationsEnabled()
+                    ) {
+                        Toast.makeText(
+                            context,
+                            getString(R.string.notificationsON),
+                            Toast.LENGTH_LONG
+                        ).show()
+                    } else {
+                        Toast.makeText(
+                            context,
+                            getString(R.string.notificationsOFF),
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
                 }
             }
         }
