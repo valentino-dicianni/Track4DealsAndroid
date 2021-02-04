9202024eb0df333fa07b15c5a64e84d17d3a8b46
daniele.gerbaldo <daniele.gerbaldo@edu.unito.it>
2021-02-04 17:07:30 +0100

    Comment added

--- app/src/main/java/com/example/track4deals/data/models/FirebaseOperationResponse.kt
+++ app/src/main/java/com/example/track4deals/data/models/FirebaseOperationResponse.kt
@@ -1,5 +1,10 @@
 package com.example.track4deals.data.models
-
+/**
+ * Data Class used for representing the result of Firebase operation
+ * @param status indicate if the operation was successful or not
+ * @param type indicate the type of operation performed on Firebase represented as FirebaseOperation enum
+ * @param message message returned by firebase if any
+ */
 data class FirebaseOperationResponse(
     val status : Boolean,
     val type: FirebaseOperation,
